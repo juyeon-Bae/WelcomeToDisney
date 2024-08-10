52ee80c7e026adc2f093ef86629a9f9e4b4ddcf3
juyeon Bae <w2316@e-mirim.hs.kr>
2024-08-10 17:25:02 +0900

    feat : add nav side bar 반응형

--- js/nav.js
+++ js/nav.js
@@ -1,11 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
     const navToggle = document.querySelector('.nav-toggle'); // 햄버거 버튼
-    const navSide = document.querySelector('.nav-side'); // 사이드 메뉴
+    const navMenu = document.querySelector('.nav-menu'); // menu item
     const hamburgerIcon = navToggle.querySelector('i'); // 햄버거 아이콘
 
     navToggle.addEventListener('click', () => {
-        // 사이드 메뉴와 아이콘 상태 토글
-        navSide.classList.toggle('toggleActive');
+        navMenu.classList.toggle('toggleActive');
         hamburgerIcon.classList.toggle('bi-list');
         hamburgerIcon.classList.toggle('bi-x-lg');
     });
